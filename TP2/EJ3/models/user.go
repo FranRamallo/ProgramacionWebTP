207a1d0d191d66fd48fd15e1800ff715b6c38adf
Jeronimo Pourtau <101447160+Jeronit1@users.noreply.github.com>
2026-09-12 21:17:09 -0300

    Delete TP2 directory

--- TP2/EJ3/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type User struct {
-	gorm.Model // Incluye ID, CreatedAt, UpdatedAt, DeletedAt
-	Name       string
-	Email      string `gorm:"unique"` // Crea una restricción UNIQUE en la BD
-}
--- TP2/go.mod
+++
@@ -1,5 +0,0 @@
-module tp2.com/TP2
-
-go 1.26.5
-
-require github.com/lib/pq v1.12.3
